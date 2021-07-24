ba79cc860d31c5950be744b91888d5339b3dd7fc
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2021-07-24 19:16:17 +0530

    add conditional render cash

---
+++ src/AfterNext.js
@@ -0,0 +1,10 @@
+const AfterNext = () => {
+    return ( 
+     <div className="after-next">
+        <h2>Cash Given:</h2>
+        <input type="text"/>
+        <button className="next-btn">Check</button>
+     </div> );
+}
+ 
+export default AfterNext;
--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-
+import {useState} from 'react';
+import AfterNext from './AfterNext';
 
 function App() {
+
+  const [nextClicked, setNextClicked] = useState(false)
+
+  const billChange = (e) =>{
+    console.log(e.target.value)
+    
+  }
+
+  const nextButtonHandler = () =>{
+    setNextClicked(true)
+  }
+  
   return (
     <div className="App">
       <h1>hello</h1>
+      <p>Enter the bill amount and cash given by the customer and know minimum number of notes to return.</p>
+      <h2>Bill Amount:</h2>
+      <input type="text" onChange={billChange}/>
+      {nextClicked ? null : <button className="next-btn" onClick={nextButtonHandler}>Next</button>}
+      {nextClicked ? <AfterNext/> : null}
+     
     </div>
   );
 }
