93f0a9f1990d1387dcba350c643d2b52aff6b7c2
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2021-07-25 15:36:05 +0530

    add afterNext component

--- src/AfterNext.js
+++ src/AfterNext.js
@@ -1,9 +1,55 @@
-const AfterNext = () => {
+import { useState } from "react";
+
+const AfterNext = ({bill}) => {
+   
+   const [cash, setCash] = useState("");
+   const [checkClicked, setCheckClicked] = useState(false);
+   const remainingAmt = cash-bill;
+
+   const cashHandler = (e) =>{
+      setCash(e.target.value)
+   }
+
+   const checkButtonHandler = () =>{
+      setCheckClicked(true)
+   }
+
     return ( 
      <div className="after-next">
         <h2>Cash Given:</h2>
-        <input type="text"/>
-        <button className="next-btn">Check</button>
+        <input type="text" onInput={cashHandler} />
+        
+        <button className="next-btn" onClick={checkButtonHandler}>Check</button>
+        {checkClicked ? (remainingAmt>=0 ?
+                          <div>remaining amount={remainingAmt}</div> 
+                                          :<div>
+                                                <p>Cash is less than bill, please enter the right amount.</p>
+                                           </div>) 
+                      :null }
+         <table>
+            <tbody>
+               <tr>
+                  <th>No.of Notes</th>
+                  <td className="noOfNotes"></td>
+                  <td className="noOfNotes"></td>
+                  <td className="noOfNotes"></td>
+                  <td className="noOfNotes">2</td>
+                  <td className="noOfNotes"></td>
+                  <td className="noOfNotes">1</td>
+                  <td className="noOfNotes"></td>
+               </tr>
+               <tr>
+                  <th>Note</th>
+                  <td>2000</td>
+                  <td>500</td>
+                  <td>100</td>
+                  <td>20</td>
+                  <td>10</td>
+                  <td>5</td>
+                  <td>1</td>
+               </tr>
+            </tbody>
+         </table>
      </div> );
 }
  
--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import AfterNext from './AfterNext';
 
 function App() {
 
-  const [nextClicked, setNextClicked] = useState(false)
+  const [nextClicked, setNextClicked] = useState(false);
+  const [bill, setBill] = useState("");
 
   const billChange = (e) =>{
-    console.log(e.target.value)
-    
+    setBill(e.target.value);  
   }
 
   const nextButtonHandler = () =>{
@@ -19,9 +19,8 @@ function App() {
       <h1>hello</h1>
       <p>Enter the bill amount and cash given by the customer and know minimum number of notes to return.</p>
       <h2>Bill Amount:</h2>
-      <input type="text" onChange={billChange}/>
-      {nextClicked ? null : <button className="next-btn" onClick={nextButtonHandler}>Next</button>}
-      {nextClicked ? <AfterNext/> : null}
+      <input type="text" value={bill} onInput={billChange}/>
+      {nextClicked ? <AfterNext bill={bill}/> : <button className="next-btn" onClick={nextButtonHandler}>Next</button>}
      
     </div>
   );
